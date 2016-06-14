d68fbe167254c6fbb287466fa7ac10696712b901
demianoleksandr <demianoleksandr@Lab06_02.itseinaudi.local>
2016-06-14 08:23:19 +0200

    LogReg dialog funziona di nuovo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-<link rel="stylesheet" type="text/css" href="peer_Education/style.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="peer_Education/functionsJs.js"></script>
 <script
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
