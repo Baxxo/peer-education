b60911c753b4c749fa4eb84eccda272e45294226
bassomatteo <bassomatteo@Lab06_01.itseinaudi.local>
2016-06-14 12:39:25 +0200

    asd

--- peer_Education/database.php
+++ peer_Education/database.php
@@ -1,102 +1,95 @@
 <?php
-
-$request = $_POST['request'];
+$request = $_POST ['request'];
 $success = "SUCCESS";
 $failed = "FAILED";
 
-switch($request){
-    case "caricaUtenti":
-        caricaUtenti();
-        break;
-    case "registrati":
-        registrati();
-        break;
-    case "login":
-        Login();
-        break;
-    case "logOut":
-    	LogOut();
-    	break;
-    default:
-        echo "Richiesta strana: " .$request;
-        break;
+switch ($request) {
+	case "caricaUtenti" :
+		caricaUtenti ();
+		break;
+	case "registrati" :
+		registrati ();
+		break;
+	case "login" :
+		Login ();
+		break;
+	case "logOut" :
+		LogOut ();
+		break;
+	default :
+		echo "Richiesta strana: " . $request;
+		break;
 }
-
-function caricaUtenti(){
+function caricaUtenti() {
 	global $failed;
 	global $success;
 	
-    $mysqli = mysqli_connect('127.0.0.1', 'root', '', 'peer');
-    $carica = mysqli_query($mysqli, 'SELECT * FROM Utente');
-
-
-    echo '<table border = "1px solid black" id = "Tabella">';
-    echo '<tr>';
-    echo '<td> Nome </td><td> Cognome </td><td> Classe </td><td> Istituto </td>';
-    echo '</tr>';
-
-    while($res = mysqli_fetch_assoc($carica)){
-        echo '<tr>';
-        echo '<td>'. $res['nome']. '</td>';
-        echo '<td>'. $res['cognome']. '</td>';
-        echo '<td>'. $res['classe']. '</td>';
-        echo '<td>'. $res['scuola']. '</td>';
-        echo '</tr>';        
-    }    
-    echo "</table>";
+	$mysqli = mysqli_connect ( '127.0.0.1', 'root', '', 'peer' );
+	$carica = mysqli_query ( $mysqli, 'SELECT * FROM Utente' );
+	
+	echo '<table border = "1px solid black" id = "Tabella">';
+	echo '<tr>';
+	echo '<td> Nome </td><td> Cognome </td><td> Classe </td><td> Istituto </td>';
+	echo '</tr>';
+	
+	while ( $res = mysqli_fetch_assoc ( $carica ) ) {
+		echo '<tr>';
+		echo '<td>' . $res ['nome'] . '</td>';
+		echo '<td>' . $res ['cognome'] . '</td>';
+		echo '<td>' . $res ['classe'] . '</td>';
+		echo '<td>' . $res ['scuola'] . '</td>';
+		echo '</tr>';
+	}
+	echo "</table>";
 }
-
-function registrati(){
+function registrati() {
 	global $failed;
 	global $success;
 	
-    $mysqli = mysqli_connect('127.0.0.1', 'root', '', 'peer');
-    $nome = $_POST['nome'];
-    $cognome = $_POST['cognome'];
-    $classe = $_POST['classe'];
-    $scuola = $_POST['scuola'];
-    $data = $_POST['data'];
-    $pass = $_POST['pass'];
-    $tel = $_POST['tel'];
-    $mail = $_POST['mail'];
-
-    $sql = "INSERT INTO Utente VALUES (null, '$nome', '$cognome', '$classe', '$scuola', '$mail', '$tel', '$data', '$pass')";
-    if($carica = mysqli_query($mysqli, $sql)){
-    	echo $success;
-    } else {
-    	echo $failed;
-    }
+	$mysqli = mysqli_connect ( '127.0.0.1', 'root', '', 'peer' );
+	$nome = $_POST ['nome'];
+	$cognome = $_POST ['cognome'];
+	$classe = $_POST ['classe'];
+	$scuola = $_POST ['scuola'];
+	$data = $_POST ['data'];
+	$pass = $_POST ['pass'];
+	$tel = $_POST ['tel'];
+	$mail = $_POST ['mail'];
+	
+	$sql = "INSERT INTO Utente VALUES (null, '$nome', '$cognome', '$classe', '$scuola', '$mail', '$tel', '$data', '$pass')";
+	if ($carica = mysqli_query ( $mysqli, $sql )) {
+		echo $success;
+	} else {
+		echo $failed;
+	}
 }
-
-function Login(){
+function Login() {
 	global $failed;
 	global $success;
 	
-    $mysqli = mysqli_connect('127.0.0.1', 'root', '', 'peer');
-    
-    $email = $_POST['mail'];
-    $pass = $_POST['pass'];
-    $carica = mysqli_query($mysqli, "SELECT nome AS Nome, email, password, id AS id FROM Utente WHERE email = '$email' AND password = '$pass'");
-    
-    $res = mysqli_fetch_object($carica);
-    if($res){
-    	session_start();
-    	$_SESSION["user_id"] = $res->id;
-    	$_SESSION["user_name"] = $res->Nome;
-    	echo $success;
-    } else {
-    	echo $failed; 
-    }
-    
+	$mysqli = mysqli_connect ( '127.0.0.1', 'root', '', 'peer' );
+	
+	$email = $_POST ['mail'];
+	$pass = $_POST ['pass'];
+	$carica = mysqli_query ( $mysqli, "SELECT nome AS Nome, email, password, id AS id FROM Utente WHERE email = '$email' AND password = '$pass'" );
+	
+	$res = mysqli_fetch_object ( $carica );
+	if ($res) {
+		session_start ();
+		$_SESSION ["user_id"] = $res->id;
+		$_SESSION ["user_name"] = $res->Nome;
+		echo $success;
+	} else {
+		echo $failed;
+	}
 }
-
-function LogOut(){
+function LogOut() {
 	global $failed;
 	global $success;
 	
-	session_start();
-	unset($_SESSION["user_id"]);
-	unset($_SESSION["user_name"]);
+	session_start ();
+	unset ( $_SESSION ["user_id"] );
+	unset ( $_SESSION ["user_name"] );
 	
 	echo $succes;
 }
--- peer_Education/functionsJS.js
+++ peer_Education/functionsJS.js
@@ -1,30 +1,32 @@
 var success = "SUCCESS";
 var failed = "FAILED";
 
-function caricaDataTabella(){
-	if($("#Tabella").length){
+function caricaDataTabella() {
+	if ($("#Tabella").length) {
 		$("#Tabella").remove();
 	} else {
 		$.ajax({
-			type: 'post',
-			url: 'peer_Education/database.php',
-			data: ({request:'caricaUtenti'}),
-			success: function (response) {
+			type : 'post',
+			url : 'peer_Education/database.php',
+			data : ({
+				request : 'caricaUtenti'
+			}),
+			success : function(response) {
 				$("body").append(response);
 			}
 		});
 	}
 }
 
-function registrati(utente){
+function registrati(utente) {
 	$.ajax({
-		type: 'post',
-		url: 'peer_Education/database.php',
-		data: (utente),
-		success: function (response) {
-			if(response == succes){
+		type : 'post',
+		url : 'peer_Education/database.php',
+		data : (utente),
+		success : function(response) {
+			if (response == succes) {
 				Materialize.toast('Grazie per la registrazione', 1500);
-			} else if(response == failed){
+			} else if (response == failed) {
 				Materialize.toast('Registrazione fallita', 1500);
 			} else {
 				Materialize.toast("C'è qualcosa che non va", 1500);
@@ -33,50 +35,56 @@ function registrati(utente){
 	});
 }
 
-function Login(dati){
+function Login(dati) {
 	$.ajax({
-		type: 'post',
-		url: 'peer_Education/database.php',
-		data: (dati),
-		success: function (response) {
-			if(response == failed){ Materialize.toast("L'email e/o la password sono sbagliati", 1500); }
-			if(response == success){ LoadPage("utente.php"); }
+		type : 'post',
+		url : 'peer_Education/database.php',
+		data : (dati),
+		success : function(response) {
+			if (response == failed) {
+				Materialize.toast("L'email e/o la password sono sbagliati",
+						1500);
+			}
+			if (response == success) {
+				LoadPage("utente.php");
+			}
 		}
 	});
 }
 
-function LogOut(){
+function LogOut() {
 	$.ajax({
-		type: 'post',
-		url: 'peer_Education/database.php',
-		data: ({request:'logOut'}),
-		success: function (response) {
+		type : 'post',
+		url : 'peer_Education/database.php',
+		data : ({
+			request : 'logOut'
+		}),
+		success : function(response) {
 			LoadPage("index.php");
 		}
 	});
 }
 
-function LoadPage(dir){
-	
+function LoadPage(dir) {
+
 	window.location.href = dir;
-	
+
 }
 
-function RegOpen(){
-	$( "#registrazione" ).fadeIn(250);
+function RegOpen() {
+	$("#registrazione").fadeIn(250);
 	$(" #footer ").fadeOut(250);
 }
-function LoginOpen(){
-	$( "#login" ).fadeIn(250);
+function LoginOpen() {
+	$("#login").fadeIn(250);
 	$(" #footer ").fadeOut(250);
 }
 
-
-function TogliReg(){
-	$( "#registrazione" ).fadeOut(250);
+function TogliReg() {
+	$("#registrazione").fadeOut(250);
 	$(" #footer ").fadeIn(250);
 }
-function TogliLogin(){
-	$( "#login" ).fadeOut(250);
+function TogliLogin() {
+	$("#login").fadeOut(250);
 	$(" #footer ").fadeIn(250);
 }
