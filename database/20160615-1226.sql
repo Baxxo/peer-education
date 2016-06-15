3aca919828bd81a6e416032e91148e8290791a89
demianoleksandr <demianoleksandr@Lab06_02.itseinaudi.local>
2016-06-15 12:27:30 +0200

    BackUp di DB per Baxxxxo

---
+++ database/20160615-1226.sql
@@ -0,0 +1,157 @@
+-- MySQL dump 10.16  Distrib 10.1.13-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: peer
+-- ------------------------------------------------------
+-- Server version	10.1.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `corso`
+--
+
+DROP TABLE IF EXISTS `corso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `corso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idTutor` int(11) NOT NULL,
+  `scuola` int(11) NOT NULL,
+  `idMateria` int(11) NOT NULL,
+  `giorno` varchar(10) NOT NULL,
+  `ora` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `corso`
+--
+
+LOCK TABLES `corso` WRITE;
+/*!40000 ALTER TABLE `corso` DISABLE KEYS */;
+INSERT INTO `corso` VALUES (1,2,1,1,'Martedi','14:00:00'),(2,0,1,1,'Martedi','14:30:00'),(3,2,2,0,'Lunedi','13:45:00'),(4,9,1,0,'Venerdi','16:30:00');
+/*!40000 ALTER TABLE `corso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `iscrizioni`
+--
+
+DROP TABLE IF EXISTS `iscrizioni`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `iscrizioni` (
+  `idCorso` int(11) NOT NULL,
+  `idStudente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `iscrizioni`
+--
+
+LOCK TABLES `iscrizioni` WRITE;
+/*!40000 ALTER TABLE `iscrizioni` DISABLE KEYS */;
+INSERT INTO `iscrizioni` VALUES (4,2);
+/*!40000 ALTER TABLE `iscrizioni` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materie`
+--
+
+DROP TABLE IF EXISTS `materie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `materie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `materia` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materie`
+--
+
+LOCK TABLES `materie` WRITE;
+/*!40000 ALTER TABLE `materie` DISABLE KEYS */;
+INSERT INTO `materie` VALUES (1,'Matematica'),(2,'Storia'),(3,'Informatica');
+/*!40000 ALTER TABLE `materie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scuole`
+--
+
+DROP TABLE IF EXISTS `scuole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scuole` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scuole`
+--
+
+LOCK TABLES `scuole` WRITE;
+/*!40000 ALTER TABLE `scuole` DISABLE KEYS */;
+INSERT INTO `scuole` VALUES (1,'IIS Einaudi-Scarpa'),(2,'Liceo Levi');
+/*!40000 ALTER TABLE `scuole` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `cognome` varchar(30) NOT NULL,
+  `classe` varchar(10) NOT NULL,
+  `scuola` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `nascita` date NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utente`
+--
+
+LOCK TABLES `utente` WRITE;
+/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
+INSERT INTO `utente` VALUES (2,'Oleksandr','Demian','4B Inf',0,'oleksandrovsky@gmail.com','3423118025','1995-03-17','password'),(9,'fabio','biscaro','nessuna',0,'fabio.biscaro@gmail.com','3471474457','1974-05-04','fabio'),(10,'allah','popo','6^fre',0,'allahcdcd@arabia.com','66666666666666','0001-12-25','jesucristo');
+/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-15 12:26:38
